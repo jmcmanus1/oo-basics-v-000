a75a0fdacdea2480aa0220f1597cc691d1a6b45b
Julie  McManus <julieannesays@gmail.com>
2020-01-30 23:57:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,10 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition 
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
+  end
+
+  def cobble
   end
 end
