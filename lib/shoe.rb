11dec6e34cca64d818f9adca4d3ae5f0a717934c
Julie  McManus <julieannesays@gmail.com>
2020-01-31 00:24:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,11 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition 
+  attr_accessor :brand, :color, :size, :material, :condition
 
   def initialize(brand)
     @brand = brand
   end
 
   def cobble
-    if condition == "old"
-      condition = "new"
     puts "Your shoe is as good as new!"
-    end
   end
 end
